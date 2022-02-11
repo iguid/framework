f1094f69a52ecddf73b2907c0ea569b022b497c5
i74ifa <i74ifa@gmail.com>
2022-02-11 11:47:55 +0300

    Application Interface

---
+++ src/Support/Interface/Application.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Iguid\Support\Interface;
+
+interface Application
+{
+    /**
+     * ------------------------------
+     * get config path
+     */
+    public function configPath();
+
+    /**
+     * ------------------------------
+     * get config path
+     */
+    public function basePath();
+
+    /**
+     * ------------------------------
+     * get config path
+     */
+    public function request();
+
+}
