248057594f3bb9ad2fff433845bd72abf78f0b3c
i74ifa <i74ifa@gmail.com>
2022-02-12 22:18:26 +0300

    Add view helper

--- src/Config/helpers.php
+++ src/Config/helpers.php
@@ -1,8 +1,22 @@
 <?php
 
 use Iguid\Config\Config;
+use Iguid\View\Viewer;
 
 function config(string $value, $default = '')
 {
     return (new Config($value, $default))->get();
+}
+
+
+/**
+ * ---------------------------------------------
+ * View helper
+ * ---------------------------------------------
+ * @param string $name
+ * @return \Iguid\View\Viewer
+ */
+function view($name): \Iguid\View\Viewer
+{
+    return new Viewer($name);
 }
